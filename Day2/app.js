94bd18cc77b7222e1e215bce0be5bda3577f507e
milanbhandari047 <milanbhandari047@gmail.com>
2023-08-02 20:44:32 +0545

    import express

---
+++ Day2/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+// port no :1300 to 650000  , 1300 vanda tala system la use garxa
+
+app.listen(3000,function(){
+    console.log("Node js project has started at port 3000")
+})
