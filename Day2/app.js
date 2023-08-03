3290b0294b3ed098750046aa63673c0cbe8e27cb
milanbhandari047 <milanbhandari047@gmail.com>
2023-08-03 20:53:57 +0545

    installing nodemon and running code in localhost

--- Day2/app.js
+++ Day2/app.js
@@ -3,6 +3,26 @@ const app = express()
 
 // port no :1300 to 650000  , 1300 vanda tala system la use garxa
 
+
+
+app.get("/",(req,res) =>{
+    res.send("<h2>Hello world</h2>")
+});
+app.get("/about",(req,res)=>{
+    res.send("I am in about page")
+});
+
+app.get("/contact",(req, res)=>{
+    res.send("Contact us at milanbhandari440@gmail.com")
+})
+
+
+
 app.listen(3000,function(){
     console.log("Node js project has started at port 3000")
 })
+
+
+
+
+
